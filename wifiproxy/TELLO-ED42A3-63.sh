13dd7237fd504d274b84560c994efb015b6af2a0
amtcvx <xp31@free.fr>
2023-10-27 16:36:17 +0200

    up

--- wifiproxy/TELLO-ED42A3-63.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#Tello 63
-export DRONE_AP=TELLO-ED42A3
-export CMD_PORT=8863
-export VID_PORT=11113
-./runproxy.sh
--- wifiproxy/TELLO-ED4310-60.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#Tello 60
-export DRONE_AP=TELLO-ED4310
-export CMD_PORT=8860
-export VID_PORT=11110
-./runproxy.sh
